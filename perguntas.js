1a8a14657ebb034273a1aca20866e90960ad763f
maria latcszuk <148063338+MariaLatcszuk26@users.noreply.github.com>
2024-09-24 09:38:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Endividamento'
+    'O que e endividamento'
+    'E um conceito que se refere a situacao em que a pessoa, empresta ou assume dividas e obrigacoes financeiras que precisam ser pagas'
+)
+
+criaCartao(
+    'Endividamento'
+    'Como evitar o endividamento'
+    'elabore um orcamento, defina limites, evite compras impulsivas, planeje seus gastos e crie um fundo de Emergencia'
+)
+ 
+criaCartao(
+    'Endividamento'
+    'Vantagens do endividamento'
+    'Embora o endividamento seja visto de forma negativa, existem algumas situacoes em que pode apresentar vantagens como'
+    'Envestimento em oportunidades, Aumento de qualidade de vida, Aproveitar descontos e promocoes'
+)
+
+criaCartao(
+    'Endividamento'
+    'Como surge o endividamento'
+    'O endividamento pode surgir apartir de varias circunstancias e fatores como'
+    'Gastos excedentes, Falta de planejamento financeiro, Despesas inesperadas'
+)
