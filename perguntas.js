d6e010472e0b376c3067a7a0e62ced5ea78f7e1c
maria latcszuk <148063338+MariaLatcszuk26@users.noreply.github.com>
2024-09-30 10:33:30 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,54 @@
-criaCartao(
-    'Endividamento'
-    'O que e endividamento'
-    'E um conceito que se refere a situacao em que a pessoa, empresta ou assume dividas e obrigacoes financeiras que precisam ser pagas'
-)
+criaCartao{
+'Endividamento'
+'O que e endividamento'
+'E um conceito que se refere a situacao em que a pessoa, empresta ou assume dividas e obrigacoes financeiras que precisam ser pagas'
+}
+    
 
-criaCartao(
+criaCartao{
     'Endividamento'
     'Como evitar o endividamento'
     'elabore um orcamento, defina limites, evite compras impulsivas, planeje seus gastos e crie um fundo de Emergencia'
-)
+}
  
-criaCartao(
+criaCartao{
     'Endividamento'
     'Vantagens do endividamento'
     'Embora o endividamento seja visto de forma negativa, existem algumas situacoes em que pode apresentar vantagens como'
     'Envestimento em oportunidades, Aumento de qualidade de vida, Aproveitar descontos e promocoes'
-)
+}
 
-criaCartao(
+criaCartao{
     'Endividamento'
     'Como surge o endividamento'
     'O endividamento pode surgir apartir de varias circunstancias e fatores como'
     'Gastos excedentes, Falta de planejamento financeiro, Despesas inesperadas'
-)
+}
+
+criaCartao{
+    'Endividamento'
+    'Desvantagens do endividamento'
+    'Custo de juros, estresse financeiro, possibilidade de perda de bens e impacto de credito'
+}
+
+criaCartao{
+    'Endividamento'
+    'Quais os riscos do endividamento'
+    'O endividamento pode trazer uma serie de riscos e consequencias como'
+    'Dificuldades financeiras, consequencias legais e impacto emocional'
+}
+
+criaCartao{
+    'Endividamento'
+    'Como resolver o endividamento'
+    'Resolver o endividamento  e um processo que demanda tempo e esforço  como: Ter uma renda extra, criar um orçamento e indentificar despesas desnecessarias'
+}
+
+criaCartao{
+    'Endividamento'
+    'Quais tipos de endividamento'
+    'Endividamento de consumo'
+    'Endividamento imobiliario'
+    'Endividamento empresarial'
+    'Endividamento por saude'
+}
