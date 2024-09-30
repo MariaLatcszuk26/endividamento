90d13a7b6acd5eeb1a4d630b75d36e2ccc52f269
maria latcszuk <148063338+MariaLatcszuk26@users.noreply.github.com>
2024-09-30 11:06:12 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,54 +1,53 @@
-criaCartao{
-'Endividamento'
-'O que e endividamento'
+criaCartao(
+'Endividamento',
+'O que e endividamento?',
 'E um conceito que se refere a situacao em que a pessoa, empresta ou assume dividas e obrigacoes financeiras que precisam ser pagas'
-}
-    
+)
 
-criaCartao{
-    'Endividamento'
-    'Como evitar o endividamento'
+criaCartao(
+    'Endividamento',
+    'Como evitar o endividamento?',
     'elabore um orcamento, defina limites, evite compras impulsivas, planeje seus gastos e crie um fundo de Emergencia'
-}
+)
  
-criaCartao{
-    'Endividamento'
-    'Vantagens do endividamento'
-    'Embora o endividamento seja visto de forma negativa, existem algumas situacoes em que pode apresentar vantagens como'
+criaCartao(
+    'Endividamento',
+    'Vantagens do endividamento',
+    'Embora o endividamento seja visto de forma negativa, existem algumas situacoes em que pode apresentar vantagens como',
     'Envestimento em oportunidades, Aumento de qualidade de vida, Aproveitar descontos e promocoes'
-}
+)
 
-criaCartao{
-    'Endividamento'
-    'Como surge o endividamento'
-    'O endividamento pode surgir apartir de varias circunstancias e fatores como'
+criaCartao(
+    'Endividamento',
+    'Como surge o endividamento?',
+    'O endividamento pode surgir apartir de varias circunstancias e fatores como',
     'Gastos excedentes, Falta de planejamento financeiro, Despesas inesperadas'
-}
+)
 
-criaCartao{
-    'Endividamento'
-    'Desvantagens do endividamento'
+criaCartao(
+    'Endividamento',
+    'Desvantagens do endividamento',
     'Custo de juros, estresse financeiro, possibilidade de perda de bens e impacto de credito'
-}
+)
 
-criaCartao{
-    'Endividamento'
-    'Quais os riscos do endividamento'
-    'O endividamento pode trazer uma serie de riscos e consequencias como'
+criaCartao(
+    'Endividamento',
+    'Quais os riscos do endividamento',
+    'O endividamento pode trazer uma serie de riscos e consequencias como',
     'Dificuldades financeiras, consequencias legais e impacto emocional'
-}
+)
 
-criaCartao{
-    'Endividamento'
-    'Como resolver o endividamento'
+criaCartao(
+    'Endividamento',
+    'Como resolver o endividamento?',
     'Resolver o endividamento  e um processo que demanda tempo e esforço  como: Ter uma renda extra, criar um orçamento e indentificar despesas desnecessarias'
-}
+)
 
-criaCartao{
-    'Endividamento'
-    'Quais tipos de endividamento'
-    'Endividamento de consumo'
-    'Endividamento imobiliario'
-    'Endividamento empresarial'
+criaCartao(
+    'Endividamento',
+    'Quais tipos de endividamento?',
+    'Endividamento de consumo',
+    'Endividamento imobiliario',
+    'Endividamento empresarial',
     'Endividamento por saude'
-}
+)
